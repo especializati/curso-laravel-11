95d70866a888865150cdd907530f938f1c9cdc81
Carlos Ferreira <carlos@especializati.com.br>
2024-04-08 11:08:55 -0300

    refactor: work with templates blade

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,9 +1,16 @@
-<h1>Novo Usuário</h1>
+@extends('admin.layouts.app')
 
-<form action="{{ route('users.store') }}" method="POST">
-    @csrf()
-    <input type="text" name="name" placeholder="Nome">
-    <input type="email" name="email" placeholder="E-mail">
-    <input type="password" name="password" placeholder="Senha">
-    <button type="submit">Enviar</button>
-</form>
+@section('title', 'Criar Novo Usuário')
+
+@section('content')
+    <h1>Novo Usuário</h1>
+
+    <form action="{{ route('users.store') }}" method="POST">
+        @csrf()
+        <input type="text" name="name" placeholder="Nome">
+        <input type="email" name="email" placeholder="E-mail">
+        <input type="password" name="password" placeholder="Senha">
+        <button type="submit">Enviar</button>
+    </form>
+
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Usuários</h1>
+@extends('admin.layouts.app')
 
+@section('title', 'Listagem dos Usuários')
+
+@section('content')
+    <h1>Usuários</h1>
     <a href="{{ route('users.create') }}">Novo</a>
 
     <table>
@@ -35,5 +30,4 @@
     </table>
 
     {{ $users->links() }}
-</body>
-</html>
+@endsection
