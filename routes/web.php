2c97fdf6ed189d6d1bb2f3b266b2fe0a31fabbf8
Carlos Ferreira <carlos@especializati.com.br>
2024-04-09 11:21:01 -0300

    refactor: change routes - add prefix

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,18 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\CheckIfIsAdmin;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function () {
-    Route::delete('/users/{user}/destroy', [UserController::class, 'destroy'])->name('users.destroy')->middleware(CheckIfIsAdmin::class);
-    Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::post('/users', [UserController::class, 'store'])->name('users.store');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-    Route::get('/users', [UserController::class, 'index'])->name('users.index');
-});
+Route::middleware('auth')
+    ->prefix('admin')
+    ->group(function () {
+        // Route::resource('/users', UserController::class);
+        Route::delete('/users/{user}/destroy', [UserController::class, 'destroy'])->name('users.destroy')->middleware(CheckIfIsAdmin::class);
+        Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+        Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+        Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+        Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+        Route::post('/users', [UserController::class, 'store'])->name('users.store');
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    });
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,4 +32,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
